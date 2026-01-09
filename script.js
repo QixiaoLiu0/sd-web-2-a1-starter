37345a46921f91fa8c65ed055584eb5aecfe4e94
Qixiao Liu <christal.liu008@gmail.com>
2026-01-08 22:23:13 -0700

    feat: complete exercises 5 - 6

--- script.js
+++ script.js
@@ -12,6 +12,14 @@ const characters = [
   { id: 8, name: "R2-D2", age: 33 },
   { id: 9, name: "C-3PO", age: 112 },
   { id: 10, name: "Padmé Amidala", age: 27 },
+  { id: 11, age: 1 },
+  { id: 12, age: 10 },
+];
+
+const brokenCharacters = [
+  { id: 1, age: 0 },
+  { id: 2, age: 1 },
+  { id: 3, name: "WWW", age: 1 },
 ];
 
 // broken test data for exercise 6
@@ -21,11 +29,13 @@ const characters = [
 // character name and append them to the HTML unordered list element with the id "names-list".
 const nameListContainer = document.getElementById("names-list");
 characters.forEach((ele) => {
-  // console.log(ele.name);
+  if (ele.name) {
+    console.log(ele.name);
 
-  const li = document.createElement("li");
-  li.textContent = ele.name;
-  nameListContainer.appendChild(li);
+    const li = document.createElement("li");
+    li.textContent = ele.name;
+    nameListContainer.appendChild(li);
+  }
 });
 
 // 2. Filter the characters array to find only those characters whose age property is less than 40.
@@ -35,11 +45,13 @@ const youngCharListContainer = document.getElementById("young-characters-list");
 characters
   .filter((ele) => ele.age < 40)
   .forEach((ele) => {
-    // console.log(ele.name);
+    if (ele.name) {
+      console.log(ele.name);
 
-    const li = document.createElement("li");
-    li.textContent = ele.name;
-    youngCharListContainer.appendChild(li);
+      const li = document.createElement("li");
+      li.textContent = ele.name;
+      youngCharListContainer.appendChild(li);
+    }
   });
 
 // 3. Build a reusable function that accepts an array of character objects as a parameter. Inside the function,
@@ -64,6 +76,7 @@ renderCharacterList(characters, document.getElementById("function-list"));
 // below the threshold value. For each filtered character, create an <li> element with their name and append
 // it to the target list. Call this function and render the results in the unordered list with id "age-filter-list".
 function renderFilteredCharactersByAge(characterArr, ageThreshold, targetDOM) {
+  targetDOM.innerHTML = "";
   characterArr
     .filter((char) => char.age < ageThreshold)
     .forEach((filteredChar) => {
@@ -85,9 +98,38 @@ renderFilteredCharactersByAge(
 // Before accessing the name property of each character object, check whether the "name" property exists.
 // If a character object is missing the name property, use console.error() to log a descriptive error message
 // to the console, and dynamically create and display the error message in the HTML div element with id "error-messages".
+function enhancedRenderCharList(characterArr, successDOM, errorDom) {
+  characterArr.forEach((char) => {
+    if (!char.name) {
+      let errorMsg = `Error: Character with ID ${char.id} is missing the "name" property.`;
+      console.error(errorMsg);
+      const li = document.createElement("li");
+      li.textContent = errorMsg;
+
+      errorDom.appendChild(li);
+    } else {
+      const li = document.createElement("li");
+      li.textContent = char.name;
+      successDOM.appendChild(li);
+    }
+  });
+}
+enhancedRenderCharList(
+  characters,
+  document.getElementById("error-handling-list"),
+  document.getElementById("error-messages")
+);
 
 // 6. Create a second array called "brokenCharacters" that intentionally contains objects with
 // missing name properties (e.g., objects with only id and age). Pass this broken array to your error-handling
 // functions from exercise 5. Verify that your error handling correctly identifies the missing name properties,
 // logs appropriate error messages to the console, and displays those error messages in the HTML div element with
 // id "broken-array-errors".
+const brokenSuccessList = document.getElementById("broken-array-list");
+const brokenErrorContainer = document.getElementById("broken-array-errors");
+
+enhancedRenderCharList(
+  brokenCharacters,
+  brokenSuccessList,
+  brokenErrorContainer
+);
